fc93c909bab5deff3615f693f11b118d4ae8a56d
Rhezaa <103802164+RejaaaAqmalll@users.noreply.github.com>
2023-02-08 10:24:09 +0700

    update

--- hello.php
+++ hello.php
@@ -1,24 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    </style>
+</head>
+<body>
+    <center>
+    <form method="post">
+        <h3>Pengecekan Jumlah hello</h3>
+        <input type="text" placeholder="masukan inputan" name="input">
+        <input type="submit" name="submit" value="Cek">
+    </form>
+    </center>
+</body>
+</html>
+
 <?php 
 
-$inputan = "><";
-$hasil = 0;
+if(isset($_POST['submit'])){
+$input = $_POST['input'];
+$inputan = str_split($input, 1);
+$GrupMardy = 0;
+$TotalHello = 0; 
 
-function cekinp($inputan) {
-    global $hasil;
-    global $inputan;
-    if ($inputan = "><"){
-        $hasil = $hasil + 2;
+foreach($inputan as $key => $value){
+    // looping awal bernilai 0
+    $awal = 0;
+
+    // pengecekan
+    if($value == ">"){
+        $GrupMardy++;
+    }elseif($value == "<"){
+        // menyimpan total grup
+        $GrupMardy = $GrupMardy;
+        // hasil 
+        $awal = $GrupMardy * 2;
+    } else {
+        continue;
     }
+    //  jumlah hello seluruhnya
+    $TotalHello += $awal;
+
+    }
+    echo "<p align = 'center'>Total Hello Yang Diucapkan adalah $TotalHello x</p>";
 }
-function loop($inputan){
-    global $hasil;
-    
-}
-
-cekinp($inputan);
-echo "hello yang diucapkan : ". $hasil;
-
-// echo date('H');
-
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
